b8eaa495e5169cec6e99404c8b88b2fe1d19a84b
Damian Valencia <damival.32@gmail.com>
2025-10-06 22:20:16 -0600

    Update CORS for frontend Railway deployment

--- src/config/config.js
+++ src/config/config.js
@@ -27,7 +27,9 @@ const config = {
       'http://localhost:5173',
       'http://127.0.0.1:3000',
       'http://127.0.0.1:3001',
-      'http://127.0.0.1:5173'
+      'http://127.0.0.1:5173',
+      'https://mediqueue.up.railway.app',
+      'https://mediqueue-production.up.railway.app'
     ],
     credentials: true
   },
